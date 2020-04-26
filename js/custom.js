c79e2baacd5d28b44ad1d7ba447abb3d6abe4cce
xenoavenger <47252167+xenoavenger@users.noreply.github.com>
2020-04-26 18:58:02 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,11 @@
+// Custom Script
+// Developed by: Samson.Onna
+// CopyRights : http://webthemez.com
+
+/*
+Theme by: WebThemez.com
+Note: Please use our back link in your site
+*/
 $( function() {
         var endDate = "April  27, 2016 15:03:25";
 
